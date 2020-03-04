287a5fb96f9b4042c3c6d261a09dc23cd4337139
amwanjesa <a.mwanjesa@gmail.com>
2020-03-04 14:09:33 +0100

    WIP

--- PAC.py
+++ PAC.py
@@ -1,6 +1,25 @@
 import numpy as np
-import random
 
-random.seed(23)
+np.random.seed(23)
+n = 20
 
-def label():
+features = np.random.choice(2, (10000, 20))
+true_labels = label_data(features)
+data = np.hstack((features, true_labels))
+#Testing
+# fake_data = np.random.choice(2, (10, 10))
+
+
+true_function = [(1, 1), (3, 0), (5, 1), (7, 1)]
+
+
+def classify_example(example):
+    return all([example[index] == label for index, label in true_function])
+
+def classify(features):
+    return np.array([classify_example(example) for example in features])
+
+def learning(data):
+    hypo = [1 for i in range(n)]
+    for example in data:
+        pass
