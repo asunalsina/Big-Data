af2f095005c93346703c5f5ecc89754804e79617
amwanjesa <a.mwanjesa@gmail.com>
2020-03-06 13:51:43 +0100

    small changes

--- PAC.py
+++ PAC.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 np.random.seed(23)
-n = 20
+N = 20
 
 FEATURES = np.random.choice(2, (10000, 20))
 TRUE_LABELS = classify(FEATURES)
@@ -20,6 +20,6 @@ def classify(features):
     return np.array([classify_example(example) for example in features])
 
 def learning(data):
-    hypo = [1 for i in range(n)]
+    hypo = [1 for i in range(N)]
     for example in data:
         pass
